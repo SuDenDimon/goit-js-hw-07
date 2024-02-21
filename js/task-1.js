c0650191f8a859818d8cc28ab1c6451d54fc230f
SuDenDimon <94171603+SuDenDimon@users.noreply.github.com>
2024-02-21 12:13:43 +0200

    task-2+

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,14 @@
 const numberOfCategories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${numberOfCategories.length}`);
 
-const titles = document.querySelectorAll("h2");
+const titles = categories.querySelectorAll("h2");
 
 titles.forEach((title) => {
 
   const titleName = title.textContent;
   console.log(`Category: ${titleName}`);
-  
-  const categoriesNames = title.parentElement.querySelectorAll("ul > li");
+
+  const categoriesNames = title.parentElement.querySelectorAll("li");
   console.log(`Elements: ${categoriesNames.length}`);
 
 });
