3d5564870805eb574df000545fccf1c9ff1c365d
SuDenDimon <94171603+SuDenDimon@users.noreply.github.com>
2024-02-23 18:27:07 +0200

    task-4+

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const submitForm = document.querySelector(".login-form");
 
+submitForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+  console.dir(formElements);
+
+  if (mail === "" || password === "") {
+    alert("All form fields must be filled in");
+  } else {
+    const formData = {
+      mail,
+      password,
+    };
+
+    console.log(formData);
+  }
+  event.currentTarget.reset();
+});
