0fcf05c5e99a3567097820905f8330b5b043f60f
SuDenDimon <sgp7766@gmail.com>
2025-02-16 18:53:37 +0200

    task-4-complete

--- js/task-4.js
+++ js/task-4.js
@@ -1,33 +1,22 @@
-// Напиши скрипт управління формою логіна.
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(event) {
+  event.preventDefault();
 
-// <form class="login-form">
-//   <label>
-//     Email
-//     <input type="email" name="email" />
-//   </label>
-//   <label>
-//     Password
-//     <input type="password" name="password" />
-//   </label>
-//   <button type="submit">Log in</button>
-// </form>
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value.trim();
+  const password = formElements.password.value.trim();
 
-
-
-
-// відправлення форми form.login-form повинна відбуватися за подією submit.
-// Під час відправлення форми сторінка не повинна перезавантажуватися.
-// Якщо при сабміті у формі є незаповнені поля, виводь alert з попередженням про те, що 'All form fields must be filled in'. Не додавай на інпути атрибут required, валідація має відбуватися саме через JS.
-// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт з двома властивостями, де ключ — це ім'я інпутів, а значення — відповідні значення цих інпутів, очищені від пробілів по краях. Для доступу до елементів форми використовуй властивість elements.
-// При сабміті форми виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
-
-
-// На що буде звертати увагу ментор при перевірці:
-
-// Прослуховується подія submit
-// Під час відправлення форми сторінка не перезавантажується
-// Якщо при сабміті у формі є незаповнені поля, виводиться alert
-// При сабміті в консоль виводиться об’єкт з двома властивостями, де ключі — це ім’я інпутів, а значення — відповідні значення цих інпутів, очищені від пробілів по краях
-// Після сабміту елементи форми очищаються
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+  } else {
+    const formData = {
+      email,
+      password,
+    };
+    form.reset();
+    console.log(formData);
+  }
+}
